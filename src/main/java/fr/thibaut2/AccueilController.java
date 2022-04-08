4965e927eaf541844d36de55671a07003d661da8
Syomaaa <Syoma02559830@gmail.com>
2022-04-08 11:02:11 +0200

    new version

--- src/main/java/fr/thibaut2/AccueilController.java
+++ src/main/java/fr/thibaut2/AccueilController.java
@@ -1,6 +1,8 @@
 package fr.thibaut2;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 public class AccueilController {
@@ -8,10 +10,12 @@ public class AccueilController {
     @FXML
     private Label Text_U;
     
-    @FXML
-    private void switchToPrimary() throws IOException {
+   // @FXML
+   // private void switchToPrimary() throws IOException {
         
-        Text_U.setText("coucou " + Donnee.prenom +" "+ Donnee.nom);
+      //  Text_U.setText("coucou " + Donnee.prenom +" "+ Donnee.nom);
         
-    }
+   // } 
+
+
 }
